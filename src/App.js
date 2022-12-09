8f199297ae6fb49f1a3475a8dfcb4d6f0c1c8abc
Sarah George <sarah@Sarahs-MacBook-Air-6.local>
2022-12-09 14:16:07 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
+
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -9,23 +11,39 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
+  
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  const[ItemsInCart, updateCart] = useState([]);
+  const[CartTotal,updateTotal] = useState(0);
+
+  function Click(item) {
+    updateTotal(CartTotal + item.price)
+    updateCart([...ItemsInCart, item])
+  }
+  
+
   return (
     <div className="App">
+
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
+      <div>
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem item={item} Click={Click} />
       ))}
+      </div>
 
       <div>
         <h2>Cart</h2>
         {/* TODO: render a list of items in the cart */}
+        {ItemsInCart.map((item) => <p> {item.name} : ${item.price} </p>)}
+        <h4> Total :  ${CartTotal} </h4>
       </div>
     </div>
   );
 }
 
 export default App;
+
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,13 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem(props) {
+    return <div> 
+    <h3> {props.item.name}</h3>
+    <img src = {props.item.image} alt = {props.item.description}/>
+    <p> {props.item.description} </p>
+    <p> Price: ${props.item.price} </p>
+
+    <button onClick={() => {props.Click(props.item)}}>Add To Cart</button>
+    </div>
+    
+    
+}
